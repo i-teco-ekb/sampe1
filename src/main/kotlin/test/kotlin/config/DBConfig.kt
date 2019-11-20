05976718f0c22cce1aa82857b6f135f633b75c3f
seshlol <seshlol@icloud.com>
2019-11-20 15:07:10 +0500

    StudentSpringTest failed

--- src/main/kotlin/test/kotlin/config/DBConfig.kt
+++ src/main/kotlin/test/kotlin/config/DBConfig.kt
@@ -1,18 +1,28 @@
 package test.kotlin.config
 
-import org.apache.commons.dbcp2.BasicDataSource
+import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.orm.hibernate5.HibernateTransactionManager
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean
 import org.springframework.transaction.annotation.EnableTransactionManagement
-import java.util.*
 import javax.sql.DataSource
+
 
 @EnableTransactionManagement
 @Configuration
 open class DBConfig(val context: ApplicationContext) {
+
+    @Bean
+    open fun dataSource(): DataSource {
+        val dataSourceBuilder = DataSourceBuilder.create()
+        dataSourceBuilder.driverClassName("org.h2.Driver")
+        dataSourceBuilder.url("jdbc:h2:file:./db/db")
+        dataSourceBuilder.username("sa")
+        dataSourceBuilder.password("")
+        return dataSourceBuilder.build()
+    }
 
     @Bean
     open fun sessionFactory(dataSource: DataSource) : LocalSessionFactoryBean = LocalSessionFactoryBean()
